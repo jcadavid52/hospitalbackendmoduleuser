e2237ebf44308d08b459c1741e547a614f913c0e
Camilo Cadavid - Ceiba Software <camilo.cadavid@ceiba.com.co>
2025-02-21 14:47:35 -0500

    Capa Aplicación: agrego implementación del servico en el manejador, Capa Dominio: agrego propiedades a la entidad AccountUserIdentity

--- HospitalModuleUser.Applica/Account/CommandHandler/Handler/AccountRegisterCommandHandler.cs
+++ HospitalModuleUser.Applica/Account/CommandHandler/Handler/AccountRegisterCommandHandler.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using HospitalModuleUser.Applica.Account.CommandHandler.Command;
 using HospitalModuleUser.Domain.Entities.AccountUser.Model.Dto;
+using HospitalModuleUser.Domain.Entities.AccountUser.Model.Entity;
+using HospitalModuleUser.Domain.Entities.AccountUser.Service;
 using MediatR;
 
 namespace HospitalModuleUser.Applica.Account.CommandHandler.Handler
 {
     public class AccountRegisterCommandHandler : IRequestHandler<AccountRegisterCommand, AccountUserDto>
     {
-        public Task<AccountUserDto> Handle(AccountRegisterCommand request, CancellationToken cancellationToken)
+        private readonly AccountUserResgisterService _userResgisterService;
+        public async Task<AccountUserDto> Handle(AccountRegisterCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var userDomain = new AccountUserEntity(request.fullName, request.email, request.email,
+               request.PhoneNumber, request.age, request.address);
+
+            return await _userResgisterService.ExecuteRegisterAsync(userDomain, request.confirmPassword);
         }
     }
 }
