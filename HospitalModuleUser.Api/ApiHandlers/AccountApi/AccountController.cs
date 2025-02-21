01b04c6a2b5dcadab2b6367298e4421649073909
Camilo Cadavid - Ceiba Software <camilo.cadavid@ceiba.com.co>
2025-02-21 11:20:42 -0500

    agrego controlador AccountUser y endpoint Register, agrego paquetes fluentValidation y mediatr al api

---
+++ HospitalModuleUser.Api/ApiHandlers/AccountApi/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalModuleUser.Api.ApiHandlers.AccountApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] AccountRegisterCommand command)
+        {
+            var resultRegister = await _mediator.Send(command);
+
+            return StatusCode(201, resultRegister);
+        }
+    }
+}
--- HospitalModuleUser.Api/Program.cs
+++ HospitalModuleUser.Api/Program.cs
@@ -1,10 +1,16 @@
+using System.Reflection;
 using HospitalModuleUser.Infra.Adapter.AccountUserAdapter;
 using HospitalModuleUser.Infra.DataSource;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
+
+//fluent validation
+builder.Services.AddFluentValidationAutoValidation();
+
 //context
 builder.Services.AddDbContext<DataContext>(opts =>
 {
@@ -19,8 +25,13 @@ builder.Services.AddDbContext<DataContext>(opts =>
         opts.UseSqlServer(config.GetConnectionString("db"));
     }
 });
-
+//identity
 builder.Services.AddIdentity<IdentityAccountUserAdapter, IdentityRole>().AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(Assembly.Load("HospitalModuleUser.Applica"));
+});
 
 // Add services to the container.
 
