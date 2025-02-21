7457b8084b9e7d49e7a14231e4cdaa60da754869
Camilo Cadavid - Ceiba Software <camilo.cadavid@ceiba.com.co>
2025-02-21 10:31:32 -0500

    agrego context ef core y clase para manejar las cuentas de usuario identity

---
+++ HospitalModuleUser.Infra/Adapter/AccountUserAdapter/IdentityAccountUserAdapter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HospitalModuleUser.Infra.Adapter.AccountUserAdapter
+{
+    public class IdentityAccountUserAdapter: IdentityUser
+    {
+        public string FullName { get; init; }
+        public int Age { get; init; }
+        public string Address { get; init; }
+    }
+}
---
+++ HospitalModuleUser.Infra/DataSource/DataContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalModuleUser.Infra.Adapter.AccountUserAdapter;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalModuleUser.Infra.DataSource
+{
+    public class DataContext: IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<IdentityAccountUserAdapter> AccountUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<IdentityUser>(entity =>
+            {
+                entity.ToTable("AccountUsers");
+            });
+
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable("AccountRoles");
+            });
+
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("AccountUserRoles");
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("AccountUserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("AccountUserLogins");
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("AccountRoleClaims");
+            });
+
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("AccountUserTokens");
+            });
+        }
+    }
+}
