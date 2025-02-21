1f24d6e59e92bd2f20e51f970766f5470a859998
Camilo Cadavid - Ceiba Software <camilo.cadavid@ceiba.com.co>
2025-02-21 11:05:12 -0500

    agrego contexto e identity al program.cs y agrego migracion de tablas identity a la base de datos

--- HospitalModuleUser.Api/Program.cs
+++ HospitalModuleUser.Api/Program.cs
@@ -1,4 +1,26 @@
+using HospitalModuleUser.Infra.Adapter.AccountUserAdapter;
+using HospitalModuleUser.Infra.DataSource;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+//context
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    var useInMemory = config.GetValue<bool>("UseInMemoryDatabase");
+
+    if (useInMemory)
+    {
+        opts.UseInMemoryDatabase("InMemoryDb");
+    }
+    else
+    {
+        opts.UseSqlServer(config.GetConnectionString("db"));
+    }
+});
+
+builder.Services.AddIdentity<IdentityAccountUserAdapter, IdentityRole>().AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
 
 // Add services to the container.
 
